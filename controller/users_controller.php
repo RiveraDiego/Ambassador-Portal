b81469c44580a457682ab42677a7240632485303
diego_menendez <die.menen@gmail.com>
2020-05-18 15:45:10 -0600

    Soluciona error de agregar nuevo usuario

--- controller/users_controller.php
+++ controller/users_controller.php
@@ -28,21 +28,12 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         } else if ($_POST['id_rol'] == 1) {
             $user->setStatus("Active");
         }
-        try{
-            $user->addUser();
-            header("Location: " . $_SESSION['root'] . "view/pages/users/?msg=success");
-        } catch (MySQLException $e) {
-            // other mysql exception (not duplicate key entry)
-            echo "<pre>";
-            print_r($e->getMessage());
-            echo "</pre>";
-        } catch (Exception $ex) {
-            echo "<pre>";
-            print_r($ex->getMessage());
-            echo "</pre>";
-            //header("Location: " . $_SESSION['root'] . "view/pages/users/?msg=error");
-        }
         
+        if ($user->addUser()) {
+            header("Location: " . $_SESSION['root'] . "view/pages/users/?msg=success");
+        } else {
+            header("Location: " . $_SESSION['root'] . "view/pages/users/?msg=error");
+        }
     }
     if ($_POST['opt'] == "edit") {
         $user = new User();
